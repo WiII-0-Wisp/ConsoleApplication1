ebd2e4ec3da8f1d9ed07682fad29a170654fe03d
WillOWisp <P.U.L_S.A.R@mail.ru>
2021-09-12 18:21:08 +0500

    Доработал в соответствии с заданием №2

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -11,5 +11,5 @@ int main()
     cin >> a;
     cout << "Введите b: ";
     cin >> b;
-    cout << "a + b = " << a + b << "\na - b = " << a - b << "\nb - a = " << b - a << "\n(|a| + |b|)/2 = " << (abs(a) + abs(b)) / 2 << endl;
+    cout << "a + b = " << a + b << "\na - b = " << a - b << "\nb - a = " << b - a << "\n(|a| + |b|)/2 = " << (abs(a) + abs(b)) / 2 << "\na * b = " << a * b << "\na / b = " << a / b << "\nb / a = " << b / a << endl;
 }
